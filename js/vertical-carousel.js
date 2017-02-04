45c9c26792c679ca77f78578ab36fac2f269cd6a
zak <zak@blueglobalmedia.com>
2017-02-04 11:57:26 -0700

    half of the js is done
    Needs some finishing touches but it's getting there. Need to hide the
    elements then test it out with different size inputs.

--- js/vertical-carousel.js
+++ js/vertical-carousel.js
@@ -1,14 +1,29 @@
 var carousel = $(".carousel"),
     currdeg  = 0;
+var elements = document.querySelectorAll('.vertical-carousel-form input:not([type="hidden"]):not([disabled])');
+console.log(elements[0].clientHeight);
+console.log(getTanDeg(18));
+var tranzlateZ = parseInt((elements[0].clientHeight) / (getTanDeg(18)) * .78);
+console.log(tranzlateZ);
 
-$(".next").on("click", { d: "n" }, rotate);
-$(".prev").on("click", { d: "p" }, rotate);
+$(".next").on("click", { direction: "next" }, rotate);
+$(".prev").on("click", { direction: "previous" }, rotate);
+
+for (var i = 0, element; element = elements[i++];) {
+	if (element){
+		element.style.transform = 'rotateX(' + 36 * (i - 1) + 'deg) translateZ(' + tranzlateZ + 'px)';
+		if(i > 2) {
+			element.type = 'hidden';
+		}
+		console.log(element);
+	}
+}
 
 function rotate(e){
-    if(e.data.d=="n"){
+    if(e.data.direction == "next"){
         currdeg = currdeg - 36;
     }
-    if(e.data.d=="p"){
+    if(e.data.direction == "previous"){
         currdeg = currdeg + 36;
     }
     carousel.css({
@@ -17,4 +32,9 @@ function rotate(e){
         "-o-transform": "rotateX("+currdeg+"deg)",
         "transform": "rotateX("+currdeg+"deg)"
     });
+}
+
+function getTanDeg(deg) {
+	var rad = deg * Math.PI/180;
+	return Math.tan(rad);
 }
