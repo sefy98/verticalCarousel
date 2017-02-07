bb962b5937ba0e3f332b5f1a299c5ce8b0fafd5b
zak <zak@blueglobalmedia.com>
2017-02-07 05:26:31 -0700

    HIdes the fields and while they're being rotated.
    works but need to add something for when the form has been looped
    through. almost complete and ready for production status.

--- js/vertical-carousel.js
+++ js/vertical-carousel.js
@@ -1,31 +1,27 @@
 var carousel = $(".carousel"),
-    currdeg  = 0;
+    currdeg  = 0,
+	elementPosition = 0;
 var elements = document.querySelectorAll('.vertical-carousel-form input:not([type="hidden"]):not([disabled])');
 console.log(elements[0].clientHeight);
-console.log(getTanDeg(18));
 var tranzlateZ = parseInt((elements[0].clientHeight) / (getTanDeg(18)) * .78);
-console.log(tranzlateZ);
 
 $(".next").on("click", { direction: "next" }, rotate);
 $(".prev").on("click", { direction: "previous" }, rotate);
 
-for (var i = 0, element; element = elements[i++];) {
-	if (element){
-		element.style.transform = 'rotateX(' + 36 * (i - 1) + 'deg) translateZ(' + tranzlateZ + 'px)';
-		if(i > 2) {
-			element.type = 'hidden';
-		}
-		console.log(element);
-	}
-}
+processElements(elementPosition);
 
 function rotate(e){
     if(e.data.direction == "next"){
         currdeg = currdeg - 36;
+        if(elementPosition !== 0) {
+	        elementPosition--;
+        }
     }
     if(e.data.direction == "previous"){
         currdeg = currdeg + 36;
+	    elementPosition++;
     }
+	processElements(elementPosition);
     carousel.css({
         "-webkit-transform": "rotateX("+currdeg+"deg)",
         "-moz-transform": "rotateX("+currdeg+"deg)",
@@ -37,4 +33,24 @@ function rotate(e){
 function getTanDeg(deg) {
 	var rad = deg * Math.PI/180;
 	return Math.tan(rad);
+}
+
+function processElements(elementPosition) {
+	var currentPosition = elementPosition;
+	for (var elementPosition = 0, element; element = elements[elementPosition++];) {
+		if (elementPosition >= currentPosition) {
+			if (element && element.style.transform === "") {
+				console.log(elements[elementPosition - 1].type);
+				elements[elementPosition - 1].previousType = elements[elementPosition - 1].type;
+				element.style.transform = 'rotateX(-' + 36 * (elementPosition - 1) + 'deg) translateZ(' + tranzlateZ + 'px)';
+				console.log(element);
+			}
+			element.type = element.previousType;
+			if (elementPosition > currentPosition + 2) {
+				element.type = 'hidden';
+			}
+		} else {
+			element.type = 'hidden';
+		}
+	}
 }
